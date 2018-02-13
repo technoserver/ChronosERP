49af44fe7a959b7c82a85dacf82484cde0d69c4a
John Vanderson <vanderson182@gmail.com>
2018-02-13 19:32:35 -0300

    finalizado resumo de caixa

--- src/main/java/com/chronos/controll/financeiro/relatorios/MovimentoCaixaRelatorioControll.java
+++ src/main/java/com/chronos/controll/financeiro/relatorios/MovimentoCaixaRelatorioControll.java
@@ -1,6 +1,7 @@
 package com.chronos.controll.financeiro.relatorios;
 
 import com.chronos.controll.AbstractRelatorioControll;
+import com.chronos.dto.MapDTO;
 import com.chronos.modelo.entidades.PdvMovimento;
 import com.chronos.modelo.entidades.view.ViewMovimentoCaixa;
 import com.chronos.repository.Repository;
@@ -9,6 +10,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.List;
 
 @Named
@@ -19,15 +21,61 @@ public class MovimentoCaixaRelatorioControll extends AbstractRelatorioControll i
 
     @Inject
     private Repository<ViewMovimentoCaixa> repository;
+    @Inject
+    private Repository<PdvMovimento> movimentoRepository;
     private List<ViewMovimentoCaixa> movimentos;
     private PdvMovimento movimento;
 
     private int idmovimento;
+    private List<MapDTO> pagamentos;
+
+    private BigDecimal dinheiro;
+    private BigDecimal cartaoDebito;
+    private BigDecimal cheque;
+    private BigDecimal cartaoCredito;
+    private BigDecimal ticket;
+    private BigDecimal credito;
+    private BigDecimal duplicata;
 
 
-    public void buscarMovimentos(){
-        movimentos = repository.getEntitys(ViewMovimentoCaixa.class,"idmovimento",idmovimento);
+    public void buscarMovimentos() {
+        movimentos = repository.getEntitys(ViewMovimentoCaixa.class, "idmovimento", idmovimento);
+        movimento = movimentoRepository.get(idmovimento, PdvMovimento.class);
+        dinheiro = BigDecimal.ZERO;
+        cartaoDebito= BigDecimal.ZERO;
+        cheque= BigDecimal.ZERO;
+        cartaoCredito= BigDecimal.ZERO;
+        ticket= BigDecimal.ZERO;
+        credito= BigDecimal.ZERO;
+        duplicata= BigDecimal.ZERO;
 
+        for(ViewMovimentoCaixa v : movimentos){
+
+            switch (v.getCodigoFormaPagamento()){
+
+                case "01" :
+                    dinheiro = dinheiro.add(v.getValor());
+                    break;
+                case "02" :
+                    cheque = cheque.add(v.getValor());
+                    break;
+                case "03" :
+                    cartaoCredito = cartaoCredito.add(v.getValor());
+                    break;
+                case "04" :
+                    cartaoDebito = cartaoDebito.add(v.getValor());
+                    break;
+                case "05" :
+                    ticket = ticket.add(v.getValor());
+                    break;
+                case "06" :
+                    duplicata = duplicata.add(v.getValor());
+                    break;
+                case "07" :
+                    credito = credito.add(v.getValor());
+                    break;
+            }
+        }
     }
 
 
@@ -41,5 +89,33 @@ public class MovimentoCaixaRelatorioControll extends AbstractRelatorioControll i
 
     public PdvMovimento getMovimento() {
         return movimento;
+    }
+
+    public BigDecimal getDinheiro() {
+        return dinheiro;
+    }
+
+    public BigDecimal getCartaoDebito() {
+        return cartaoDebito;
+    }
+
+    public BigDecimal getCheque() {
+        return cheque;
+    }
+
+    public BigDecimal getCartaoCredito() {
+        return cartaoCredito;
+    }
+
+    public BigDecimal getTicket() {
+        return ticket;
+    }
+
+    public BigDecimal getCredito() {
+        return credito;
+    }
+
+    public BigDecimal getDuplicata() {
+        return duplicata;
     }
 }
