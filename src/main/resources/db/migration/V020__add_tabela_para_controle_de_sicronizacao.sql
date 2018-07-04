409e5551f600e4560b2200d15ddad8d31c1769ee
john <vanderson182@gmail.com>
2018-07-04 14:21:36 -0300

    incluso model para tabela de sync pendentes

--- src/main/resources/db/migration/V020__add_tabela_para_controle_de_sicronizacao.sql
+++ src/main/resources/db/migration/V020__add_tabela_para_controle_de_sicronizacao.sql
@@ -1,8 +1,8 @@
 CREATE TABLE sync_pendentes
 (
-  id          SERIAL     NOT NULL,
-  id_caixa    INTEGER    NOT NULL,
-  codigo_acao VARCHAR(3) NOT NULL,
-  nome_acao   VARCHAR(30) DEFAULT '',
+  id           SERIAL     NOT NULL,
+  id_pdv_caixa INTEGER    NOT NULL,
+  codigo_acao  VARCHAR(3) NOT NULL,
+  nome_acao    VARCHAR(30) DEFAULT '',
   PRIMARY KEY (id)
 );
