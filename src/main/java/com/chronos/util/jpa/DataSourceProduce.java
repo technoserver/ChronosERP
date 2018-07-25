d2bec879437ac2bb7581d9625bb761446a1c915d
john <vanderson182@gmail.com>
2018-07-25 13:52:57 -0300

    ajuste dashboard e tela de movimento

--- src/main/java/com/chronos/util/jpa/DataSourceProduce.java
+++ src/main/java/com/chronos/util/jpa/DataSourceProduce.java
@@ -1,12 +1,7 @@
 package com.chronos.util.jpa;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.Default;
-import javax.enterprise.inject.Produces;
-import java.beans.PropertyVetoException;
 
 /**
  * Created by john on 02/01/18.
@@ -14,28 +9,28 @@ import java.beans.PropertyVetoException;
 @ApplicationScoped
 public class DataSourceProduce {
 
-    @Produces
-    @RequestScoped
-    @Default
-    public ComboPooledDataSource dataSource() {
-
-        ComboPooledDataSource dataSource = new ComboPooledDataSource("tenant");
-        try {
-            dataSource.setDriverClass("org.postgresql.Driver");
-        } catch (PropertyVetoException pve) {
-            System.out.println("Cannot load datasource driver (postgresql) : " + pve.getMessage());
-            return null;
-        }
-
-        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/chronosLight");
-        dataSource.setUser("aplicacao");
-        dataSource.setPassword("aplicacao");
-        dataSource.setMinPoolSize(1);
-        dataSource.setMaxPoolSize(20);
-        dataSource.setMaxIdleTime(300);
-
-
-        return dataSource;
-
-    }
+//    @Produces
+//    @RequestScoped
+//    @Default
+//    public ComboPooledDataSource dataSource() {
+//
+//        ComboPooledDataSource dataSource = new ComboPooledDataSource("tenant");
+//        try {
+//            dataSource.setDriverClass("org.postgresql.Driver");
+//        } catch (PropertyVetoException pve) {
+//            System.out.println("Cannot load datasource driver (postgresql) : " + pve.getMessage());
+//            return null;
+//        }
+//
+//        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/chronosLight");
+//        dataSource.setUser("aplicacao");
+//        dataSource.setPassword("aplicacao");
+//        dataSource.setMinPoolSize(1);
+//        dataSource.setMaxPoolSize(20);
+//        dataSource.setMaxIdleTime(300);
+//
+//
+//        return dataSource;
+//
+//    }
 }
