4e069c62f5a8ebc94de74c3d7090d53a682aa3c7
john <vanderson182@gmail.com>
2018-08-21 15:09:56 -0300

    refatorado definicao do Indicador de IE

--- src/main/java/com/chronos/controll/nfe/NfeCabecalhoControll.java
+++ src/main/java/com/chronos/controll/nfe/NfeCabecalhoControll.java
@@ -570,19 +570,7 @@ public class NfeCabecalhoControll extends AbstractControll<NfeCabecalho> impleme
 
         getObjeto().setLocalDestino(LocalDestino.getByUf(empresa.buscarEnderecoPrincipal().getUf(), pessoaCliente.getUf()));
 
-        if (getObjeto().getDestinatario().getCpfCnpj().length() == 14) {
-            if (getObjeto().getDestinatario().getInscricaoEstadual() == null || getObjeto().getDestinatario().getInscricaoEstadual().equals("")) {
-                getObjeto().getDestinatario().setIndicadorIe(9);
-
-            } else if (getObjeto().getDestinatario().getInscricaoEstadual().equalsIgnoreCase("ISENTO")) {
-                getObjeto().getDestinatario().setIndicadorIe(2);
-            } else {
-                getObjeto().getDestinatario().setIndicadorIe(1);
-            }
-
-        } else {
-            getObjeto().getDestinatario().setIndicadorIe(2);
-        }
+        nfeService.definirIndicadorIe(getObjeto().getDestinatario(), getObjeto().getModeloDocumento());
 
         dadosSalvos = false;
 
