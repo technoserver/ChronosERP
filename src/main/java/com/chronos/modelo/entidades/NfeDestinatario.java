e746dfd5a2c8d0c0f7b91361b5ddc0470df08859
john <vanderson182@gmail.com>
2018-08-21 15:04:55 -0300

    refatorado definicao do Indicador de IE

--- src/main/java/com/chronos/modelo/entidades/NfeDestinatario.java
+++ src/main/java/com/chronos/modelo/entidades/NfeDestinatario.java
@@ -179,7 +179,13 @@ public class NfeDestinatario implements Serializable {
     }
 
     /**
-     * indIEDest - Indicador da IE do Destinatário - 1=Contribuinte ICMS (informar a IE do destinatário);  2=Contribuinte isento de Inscrição no cadastro de   Contribuintes do ICMS;  9=Não Contribuinte, que pode ou não possuir Inscrição   Estadual no Cadastro de Contribuintes do ICMS;  Nota 1: No caso de NFC-e informar indIEDest=9 e não   informar a tag IE do destinatário;  Nota 2: No caso de operação com o Exterior informar   indIEDest=9 e não informar a tag IE do destinatário;  Nota 3: No caso de Contribuinte Isento de Inscrição   (indIEDest=2), não informar a tag IE do destinatário.
+     * indIEDest - Indicador da IE do Destinatário -
+     * 1=Contribuinte ICMS (informar a IE do destinatário);
+     * 2=Contribuinte isento de Inscrição no cadastro de   Contribuintes do ICMS;
+     * 9=Não Contribuinte, que pode ou não possuir Inscrição   Estadual no Cadastro de Contribuintes do ICMS;
+     * Nota 1: No caso de NFC-e informar indIEDest=9 e não   informar a tag IE do destinatário;
+     * Nota 2: No caso de operação com o Exterior informar   indIEDest=9 e não informar a tag IE do destinatário;
+     * Nota 3: No caso de Contribuinte Isento de Inscrição   (indIEDest=2), não informar a tag IE do destinatário.
      *
      * @return
      */
