a4ccaf247149539a21b646612d0d048452800928
John Vanderson <vanderson182@gmail.com>
2018-09-25 12:36:52 -0300

    ajuste migração empresa pessoa

--- src/main/resources/db/migration/V042__ajustado_tabela_empresa_pessoa.sql
+++ src/main/resources/db/migration/V042__ajustado_tabela_empresa_pessoa.sql
@@ -8,31 +8,31 @@ ALTER TABLE empresa_pessoa
   DROP CONSTRAINT empresa_pessoa_id_pessoa_fkey;
 
 ALTER TABLE empresa_pessoa
-  ADD CONSTRAINT empresa_pessoa_pkey PRIMARY KEY empresa_pessoa (id_empresa, id_pessoa);
+  ADD CONSTRAINT empresa_pessoa_pkey PRIMARY KEY (id_empresa, id_pessoa);
 
 CREATE UNIQUE INDEX id_empresa_pessoa_indx
   ON empresa_pessoa (id);
 
 ALTER TABLE empresa_pessoa
-  add column empresa_pessoa char(1) default 'N';
+  add column empresa_principal char(1) default 'N';
 
 update empresa_pessoa
-set empresa_pessoa
-where id in(select ep.id
-            from colaborador c
-                   inner join pessoa p on p.id = c.id_pessoa
-                   inner join empresa_pessoa ep on ep.id_pessoa = p.id);
+set empresa_principal = 'S'
+where id in (select ep.id
+             from colaborador c
+               inner join pessoa p on p.id = c.id_pessoa
+               inner join empresa_pessoa ep on ep.id_pessoa = p.id);
 
 update empresa_pessoa
-set empresa_pessoa
-where id in(select ep.id
-            from cliente c
-                   inner join pessoa p on p.id = c.id_pessoa
-                   inner join empresa_pessoa ep on ep.id_pessoa = p.id);
+set empresa_principal = 'S'
+where id in (select ep.id
+             from cliente c
+               inner join pessoa p on p.id = c.id_pessoa
+               inner join empresa_pessoa ep on ep.id_pessoa = p.id);
 
 update empresa_pessoa
-set empresa_pessoa
-where id in(select ep.id
-            from fornecedor f
-                   inner join pessoa p on p.id = f.id_pessoa
-                   inner join empresa_pessoa ep on ep.id_pessoa = p.id);
+set empresa_principal = 'S'
+where id in (select ep.id
+             from fornecedor f
+               inner join pessoa p on p.id = f.id_pessoa
+               inner join empresa_pessoa ep on ep.id_pessoa = p.id);
