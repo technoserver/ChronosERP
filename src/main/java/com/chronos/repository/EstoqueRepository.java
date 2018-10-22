49c008b90641a439ed9bf780c7fe729dfc50c659
john <vanderson182@gmail.com>
2018-10-22 11:32:48 -0300

    ajustes

--- src/main/java/com/chronos/repository/EstoqueRepository.java
+++ src/main/java/com/chronos/repository/EstoqueRepository.java
@@ -134,7 +134,7 @@ public class EstoqueRepository extends AbstractRepository implements Serializabl
                 "LEFT JOIN ProdutoPromocao pp on pp.produto.id = p.id " +
                 "LEFT JOIN TabelaPrecoProduto tp on tp.produto = p.id " +
                 "LEFT JOIN TabelaPreco t on t.id = tp.tabelaPreco.id " +
-                "where (LOWER(p.nome)  like ?1 or p.gtin = ?1 or p.codigoInterno = ?1) and ep.empresa.id = ?2 and  p.tributGrupoTributario is not null and p.tipo = 'V' and t.principal = 'S'";
+                "where (LOWER(p.nome)  like ?1 or p.gtin = ?1 or p.codigoInterno = ?1) and ep.empresa.id = ?2 and  p.tributGrupoTributario is not null and p.tipo = 'V'";
 
         nome = !org.apache.commons.lang3.StringUtils.isNumeric(nome) ? "%" + nome.toLowerCase().trim() + "%" : nome;
         List<ProdutoDTO> produtos = getEntity(ProdutoDTO.class, jpql, nome, empresa.getId());
