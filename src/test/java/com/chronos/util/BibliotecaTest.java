fe76afe32eb46d0ee309af5dd11c3d3405548a54
John Vanderson <vandereson182@gmail.com>
2019-05-10 19:47:53 -0300

    ajuste valor total dos tributos

---
+++ src/test/java/com/chronos/util/BibliotecaTest.java
@@ -0,0 +1,35 @@
+package com.chronos.util;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertTrue;
+
+public class BibliotecaTest {
+
+    @Test
+    public void devemos_garantir_que_os_valores_somando_fechem() {
+
+        BigDecimal valor1 = new BigDecimal("2.86267800");
+        BigDecimal valor2 = new BigDecimal("0.99360000");
+        BigDecimal valor3 = new BigDecimal("0.75036000");
+        BigDecimal valor4 = new BigDecimal("0.12420000");
+        BigDecimal valor5 = new BigDecimal("0.12");
+
+        BigDecimal valorEsperado = new BigDecimal("5.05");
+
+        BigDecimal total = BigDecimal.ZERO;
+
+        total = Biblioteca.soma(valor1, total);
+        total = Biblioteca.soma(valor2, total);
+        total = Biblioteca.soma(valor3, total);
+        total = Biblioteca.soma(valor4, total);
+        total = Biblioteca.soma(valor5, total);
+
+        int result = total.compareTo(valorEsperado);
+
+
+        assertTrue(result == 0);
+    }
+}
